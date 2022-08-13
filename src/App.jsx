0d7dba74dc330b922097305e786f752e7abd0a35
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-13 13:55:30 +0600

    working on home page

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+	Navbar,
+	Billing,
+	CardDeal,
+	Business,
+	Clients,
+	CTA,
+	Stats,
+	Footer,
+	Testimonials,
+	Hero,
+} from "./Components";
+const App = () => {
+	return (
+		<div className="bg-primary w-full overflow-hidden">
+			<div className="paddingX flexCenter">
+				<div className="boxWidth">
+					<Navbar />
+				</div>
+			</div>
+
+			<div className="bg-primary flexStart">
+				<div className="boxWidth">
+					<Hero />
+				</div>
+			</div>
+
+			<div className="bg-primary paddingX flexStart">
+				<div className="boxWidth">
+					<Stats />
+					<Business />
+					<Billing />
+					<CardDeal />
+					<Testimonials />
+					<Clients />
+					<CTA />git
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
