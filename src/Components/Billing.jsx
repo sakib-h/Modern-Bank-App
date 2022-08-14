14cf232dd30b915e451e32ed203a832a2d6a412b
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-15 05:48:54 +0600

    billing section updated

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { apple, bill, google } from "../assets";
 
 const Billing = () => {
-	return <div>Billing</div>;
+	return (
+		<section
+			id="product"
+			className="flex md:flex-row flex-col-reverse paddingY">
+			<div className="flex-1 flex flexCenter md:mr-10 mr-0 md:mt-0 mt-10 relative">
+				<img
+					src={bill}
+					alt="billing"
+					className="w-[100%] h-[100%] z-[5]"
+				/>
+				<div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+				<div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+			</div>
+
+			<div className="flex-1 flexStart flex-col">
+				<h2 className="heading2">
+					Easily control your <br className="sm:block hidden" />
+					billing & invoicing.
+				</h2>
+				<p className="paragraph max-w-[470px] mt-5">
+					Elit enim sed massa etiam. Mauris eu adipiscing ultrices
+					ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor
+					integer platea placerat.
+				</p>
+
+				<div className="flex flex-row flex-wrap mt-6 sm:mt-10">
+					<img
+						src={apple}
+						alt="apple-store"
+						className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+					/>
+					<img
+						src={google}
+						alt="google-play"
+						className="w-[128px] h-[42px] object-contain  cursor-pointer"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Billing;
