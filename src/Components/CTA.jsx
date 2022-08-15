d561b7e0753cc19b317c6906816432e1c1e577af
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-15 13:09:15 +0600

    working on the footer sectin

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
 const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+	return (
+		<section className="flexCenter marginY padding flex-col sm:flex-row bg-black-gradient-2 rounded-[20px] box-shadow">
+			<div className="flex-1 flex flex-col ">
+				<h2 className="heading2">Let’s try our service now!</h2>
+				<p className="paragraph max-w-[470px] mt-5">
+					Everything you need to accept card payments and grow your
+					business anywhere on the planet.
+				</p>
+			</div>
 
-export default CTA
+			<div className="flexCenter ml-0 sm:ml-10 mt-10 sm:mt-0">
+				<Button />
+			</div>
+		</section>
+	);
+};
+
+export default CTA;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="flexCenter paddingY flex-col ">
+			<div className="flexStart md:flex-row flex-col mb-8 w-full">
+				<div></div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
