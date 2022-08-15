b16491e52f0dc02dbed576321e0610e18d36e624
Sakib Hasan <sakib100.sa@gmail.com>
2022-08-15 12:15:33 +0600

    client section complete

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { clients } from "../constants";
 const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+	return (
+		<section className="flexCenter my-4 ">
+			<div className="flexCenter flex-wrap w-full">
+				{clients.map((client) => (
+					<div
+						key={client.id}
+						className="flex-1 flexCenter min-w-[120px] sm:min-w-[192px] ">
+						<img
+							src={client.logo}
+							alt="client"
+							className="w-[100px] sm:w-[192px] object-contain cursor-pointer hover:brightness-[200%]"
+						/>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
 
-export default Clients
+export default Clients;
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => {
-	return <div>Testimonials</div>;
+	return (
+		<section id="clients" className="paddingY flexCenter flex-col relative">
+			<div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+
+			<div className="w-full flex justify-between items-center flex-col md:flex-row mb-6 sm:mb-16 relative z-[1]">
+				<h1 className="heading2">
+					What people are <br className="sm:block hidden" /> saying
+					about us
+				</h1>
+				<div className="w-full md:mt-0 mt-6">
+					<p className="paragraph text-left max-w-[450px]">
+						Everything you need to accept card payments and grow
+						your business anywhere on the planet.
+					</p>
+				</div>
+			</div>
+
+			<div className="flex flex-wrap justify-center sm:justify-start w-full feedback-container relative z-[1]">
+				{feedback.map((card) => (
+					<FeedbackCard key={card.id} {...card} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Testimonials;
